d71119241974cd5fc4df9b0e054a0c9dc356d4c8
YuvaBalaji01 <yuvabalajikumar@gmail.com>
2026-06-05 15:49:12 +0530

    3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL =
+  "https://tdc-assn.onrender.com/";
 
 export async function getCustomers() {
   const response = await fetch(`${BASE_URL}/customers`);
